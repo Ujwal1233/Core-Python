70f1b75b5d626461a8de1db7bff0d15757b2829f
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-10 08:43:33 +0530

    Updated project files

---
+++ Even or odd in user defined range separately.py
@@ -0,0 +1,18 @@
+def isEVENODD(n):
+    return n%2==0
+sr=int(input("Enter a sr num:"))
+er=int(input("Enter a er num:"))
+if sr>er:
+       print("Invalid output.")
+else:
+       print("Even num:",end=" ")
+for i in range(sr,er+1):
+    flag=isEVENODD(i)
+    if flag:
+        print(i,end=" ")
+print("\nOdd num:",end=" ")
+for i in range(sr,er+1):
+    flag=isEVENODD(i)
+    if not flag:
+        print(i,end=" ")
+        
