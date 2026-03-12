eccea8aaa2a82716cdeaab31d8161bcb6e0e353d
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-12 14:06:56 +0530

    Updated project files

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,15 +1,21 @@
 class Book:
-    def __init__(self,value):
-        self.__pages=value
-def setter(self,value):
-    if(value>=1):
-        self.__pages=value
+    def __init__(self, value):
+        self.__pages = value
+
+    def setter(self, value):
+        if value >= 1:
+            self.__pages = value
+
     def getter(self):
         return self.__pages
-    b1=Book(100)
-    b1.setter(114)
-    res=b1.getter()
-    print(res)
-    b1.setter(-99)
-    res1=b1.getter()
-    print(res1)
+
+
+b1 = Book(100)
+
+b1.setter(114)
+res1 = b1.getter()
+print(res1)
+
+b1.setter(115)
+res1 = b1.getter()
+print(res1)
