1d5cfdd7e5bc1014567b2bc45df1b856699be4b1
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-12 10:08:07 +0530

    Updated project files

---
+++ LCM Using Reccursion.py
@@ -0,0 +1,11 @@
+def rec_lcm(n,m,lcm):
+    if lcm>m*n:
+        return lcm
+    if lcm%m==0 and lcm%n==0:
+        return lcm
+    return rec_lcm(n,m,lcm+1)
+data1=int(input("enter num "))
+data2=int(input("enter num "))
+lcm=max(data1,data2)
+res=rec_lcm(data1,data2,lcm)
+print(res)
