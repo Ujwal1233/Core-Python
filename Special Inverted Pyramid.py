6fde41f6d4aae94f3ec35ddc3689d80b7837a5fd
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-16 10:02:02 +0530

    Updated project files

---
+++ Special Inverted Pyramid.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a num:"))
+for i in range(n,(1-1),-1):
+     for k in range(n,i,-1):
+         print(" ",end=" ")
+     for j in range(1,(i*2)):
+         print("*",end=" ")
+     print()
