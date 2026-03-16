101a66bba8721b17a780dffd61f178d713ecfe85
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-16 10:20:09 +0530

    Updated project files

---
+++ LHS Parallelogram.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a num:"))
+for i in range(n,(1-1),-1):
+     for k in range(n,i,-1):
+         print(" ",end="")
+     for j in range(1,n+1):
+         print("*",end="")
+     print()
