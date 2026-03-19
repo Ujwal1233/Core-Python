187d9e13e9d9ad7fbc3d592427091a83e68548d3
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-19 17:18:05 +0530

    Updated project files

---
+++ LHSide Right angle triangle.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a num"))
+for i in range(1,(n+1)):
+    for j in range(1,(n+1)):
+        if (i==n or j==n or (i+j)==(n+1)):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+    
