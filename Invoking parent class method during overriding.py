0a803ce775101867617071a05d601cad5c930a76
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-23 13:52:26 +0530

    Updated project files

---
+++ Invoking parent class method during overriding.py
@@ -0,0 +1,21 @@
+class A:
+    def __init__(self):
+        self.a=10
+    def display(self):
+        print("Inside A")
+class B(A):
+    def display(self):
+        print("Inside B")
+class C(B):
+    def display(self):
+        print("Inside C")
+class D(C):
+    def dispD(self):
+        C.display(self)
+        B.display(self)
+        A.display(self)
+d1=D()
+d1.dispD()
+
+
+    
