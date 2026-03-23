20d6b84f3b636e5386a463e9b2cc263fe3482366
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-23 13:37:38 +0530

    Updated project files

---
+++ Invoking a parent class constructor using inbuilt function.py
@@ -0,0 +1,15 @@
+class A:
+    def __init__(self):
+        self.a=10
+class B(A):
+    def __init__(self):
+        super().__init__()
+        self.b=20
+class C(B):
+    def __init__(self):
+        super().__init__()
+        self.c1=30
+cf=C()
+print(cf.c1)
+print(cf.b)
+print(cf.a)
