f63c613a9f99258f9e6ff5b8bf3638af1a13bc61
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-25 14:16:45 +0530

    Updated project files

---
+++ Handling a specific exception.py
@@ -0,0 +1,14 @@
+try:
+    print("Enter a value 1")
+    a=int(input())
+    print("Enter a value 2")
+    b=int(input())
+    c=a/b
+    print(c)
+except ValueError as e:
+    print("It is value error")
+except ZeroDivisionError as e:
+    print("It is a ZerodivisionError")
+except Exception as e:
+    print("It is a error")
+print("Program end")
