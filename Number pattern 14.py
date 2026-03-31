367cededb94c6707de411b5ccb5245d97a418daa
Ujwal D <ujwaldhananjaya@gmail.com>
2026-03-31 14:58:40 +0530

    Updated project files

--- Number pattern 14.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter the number: "))
-for i in range(n,1-1,-1):
-    for j in range(i,i+n):
-         print(j, end=" ")
-    print()
--- Number pattern 5.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter a num:"))
-for i in range(n,1-1,-1):
-    for j in range(n,i-1,-1):
-        print(j,end=" ")
-    print()
-    
