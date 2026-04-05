6355ca5c83560a6a07759a8df47c73e3ac212473
Ujwal D <ujwaldhananjaya@gmail.com>
2026-04-05 12:22:17 +0530

    Updated project files

---
+++ __pycache__/Tuples in python.py
@@ -0,0 +1,20 @@
+# # Tuple Features
+# ->Tuples are Hetergeneous
+# ->Dynamic
+# ->Insertion Order
+# ->Duplicates Allowed
+# ->Immutable
+
+# Syntax:
+# tuple_name=(value1,value2,value3,.....)
+# Example
+t1=(10,20,30,40)
+print(t1)
+print(type(t1))
+t2=(10,20.2,"Rama")
+t2=()
+t3=(10)
+print(t3)
+print(type(t3))
+t4=(10,)
+print(type(t4))
