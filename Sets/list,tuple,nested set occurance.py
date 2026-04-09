c49f019bf6ceaa9065f36ca53b9ed236da2a888c
Ujwal D <ujwaldhananjaya@gmail.com>
2026-04-09 14:01:59 +0530

    Updated project files

---
+++ Sets/list,tuple,nested set occurance.py
@@ -0,0 +1,6 @@
+s1={10,20,[30,40],50}
+print(s1)#List is not allowed in set
+s2={(10,20),30,40,50}
+print(s2)#Tuple is allowed in set
+s3={10,20,{30,40},50}
+print(s3)#Nested set is not allowed in set
