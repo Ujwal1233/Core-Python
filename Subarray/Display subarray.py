3ea2d744a55c54c914cd60f5f92d68abc8dfcdb2
Ujwal D <ujwaldhananjaya@gmail.com>
2026-04-21 09:44:45 +0530

    Updated project files

--- Subarray/Display subarray.py
+++ Subarray/Display subarray.py
@@ -13,3 +13,19 @@ def displaySubarray(arr):
         for j in range(i,len(arr)):
             res.append(arr[j])
             print(res)
+arr=createIntarr()
+print("The original array",arr)
+displaySubarray(arr)
+
+
+def CreateSubarrays(arr):
+    res=[]
+    for i in range(0,len(arr)):
+        sub=[]
+        for k in range(i,j+1):
+            sub.append(arr[k])
+        res.append(sub)
+    return res
+arr=createIntarr()
+print("The original array",arr)
+displaySubarray(arr)
